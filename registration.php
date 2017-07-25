7876216dc3cc58321f611de579f84b0d0c419922
PHPCuong <bestearnmoney87@gmail.com>
2017-07-25 18:25:18 +0700

    init source code

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @Author: Ngo Quang Cuong
+ * @Date:   2017-07-25 17:42:29
+ * @Last Modified by:   nquangcuong
+ * @Last Modified time: 2017-07-25 17:42:37
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PHPCuong_AjaxAddToCart',
+    __DIR__
+);
